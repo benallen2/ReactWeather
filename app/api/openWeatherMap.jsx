c0f21347fc2d305768c9f3c45c3764d8d84bddd9
Ben Allen <ben.allen2@gmail.com>
2016-12-09 14:54:21 -0500

    added error modal

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -10,26 +10,19 @@ module.exports = {
     var requestUrl = `${OPEN_WEATHER_MAP_URL}${encodedLocation}.json`;
 
     return axios.get(requestUrl).then(function (res) {
-      if(res.data.cod && res.data.message){
-        throw new Error(res.data.message);
+      if(res.data.response.error && res.data.response.error.description){
+        throw new Error(res.data.response.error.description);
       } else {
         console.log(res.data);
           var weatherData = [];
-          var temp = res.data.current_observation.temperature_string;
           var weather = res.data.current_observation.weather;
-          var graphic = res.data.current_observation.icon_url
+          var temp = res.data.current_observation.temperature_string;
+          var graphic = res.data.current_observation.icon_url;
           weatherData = [temp, weather, graphic];
           return weatherData
-
-        // console.log(res.data);
-        // var temp = res.data.current_observation.temperature_string;
-        // var weather = res.data.current_observation.weather;
-        // console.log(temp);
-        // console.log(weather);
-        // return temp, weather
       }
     }, function (res) {
-      throw new Error(res.data.message);
+      throw new Error(res.data.response.error.description);
     });
   }
 }
