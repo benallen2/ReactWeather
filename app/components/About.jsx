a00c46a82a46ed7b6601376f0605eedc31f2ae5f
Ben Allen <ben.allen2@gmail.com>
2016-12-09 13:16:25 -0500

    added about resources and styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,17 @@ var React = require('react');
 var About = (props) => {
   return(
     <div>
-      <h3>About Component</h3>
-      <p>Welcome to the about page</p>
+      <h1 className="text-center">About</h1>
+      <p className="text-center">This app was created using React, Foundation, Webpack, and the wunderground weather API</p>
+      <p className="text-center">Here are some of the tools used:</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the JavaScript Framework used.
+        </li>
+        <li>
+          <a href="https://www.wunderground.com/weather/api/">Wunderground API</a> - I used this API to pull the temperature data.
+        </li>
+      </ul>
     </div>
   )
 };
