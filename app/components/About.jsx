c3fd284155cfd11374f3f751667769959e020046
Ben Allen <ben.allen2@gmail.com>
2016-12-09 15:29:47 -0500

    add styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,8 +4,8 @@ var About = (props) => {
   return(
     <div>
       <h1 className="text-center page-title">About</h1>
-      <p className="text-center">This app was created using React, Foundation, Webpack, and the wunderground weather API</p>
-      <p className="text-center">Here are some of the tools used:</p>
+      <p>This app was created using React, Foundation, Webpack, and the wunderground weather API</p>
+      <p>Here are some of the tools used:</p>
       <ul>
         <li>
           <a href="https://facebook.github.io/react">React</a> - This was the JavaScript Framework used.
